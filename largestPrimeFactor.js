c5d2b27593c181ee2c612f303984cdba4a8200db
Aukai Tirrell <mtirrell@hawaii.edu>
2016-11-19 11:18:13 -1000

    went through tests with jon, can only pass first test

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,29 @@
-exports.largestPrimeFactor = function(n){
-  var primeNumber = 0;
-  // do your work here
+exports.largestPrimeFactor = function(factoredNum){
 
-  return primeNumber;
+  function isPrime(num) {
+    //go down every number betwen 2 and num, if any are divisible (mod i === 0)
+    // memoization
+    //evaluating the Big-O notation of the algorithm(s)
+    for (var i = 2; i <= num/ 2; i++) {
+      if ( num % i === 0) {
+        //found a factor is not prime
+        return false;
+      }
+      }
+      return true;
+    }
+
+
+  var primeNumber = 0;
+  //for loop from 2 to n
+  for (var i = 2; i < factoredNum; i++) {
+    //must be evenly divisible by n
+    //must also be prime
+    //then set primeNubmer to i
+    if (factoredNum % i === 0 && isPrime(i)) {
+      primeNubmer = i;
+    }
+  }
+  return primeNubmer;
 };
+
